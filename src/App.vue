2bfe24b58d8d8246826f72318178801f0583fe43
jack <jack68@naver.com>
2022-07-07 17:11:45 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader> 
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" 
+              v-on:removeTodoItem="removeOneItem" 
+              v-on:toggleTodoItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:clearAllTodo="clearAllItems"></TodoFooter>  
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoInput from './components/TodoInput.vue'
+
+export default {
+  data: function(){
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem){
+        var obj = {completed: false, item: todoItem}
+        console.log(todoItem);
+        //저장로직수행
+        localStorage.setItem(todoItem, JSON.stringify(obj));
+        this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem,index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index,1);
+    },
+    toggleOneItem: function(todoItem,index){
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      // localStorage는 업데이트가 없으므로 해당 ITEM을 삭제 후, 재정의하여 업데이트 처리를 한다..
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
+    },
+    clearAllItems: function(){
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created:function(){
+      if(localStorage.length >= 0){
+        for(var i = 0 ; i < localStorage.length ; i ++){
+          if(localStorage.key(i) !== "loglevel:webpack-dev-server"){
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          }
+        }
+      }
+  },
+  components:{
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+    <AlertModal v-if="showModal" @close="showModal = false">
+        <template v-slot:header>경고</template>
+        <template v-slot:body>아무 것도 입력하지 않았습니다.</template>
+        <template v-slot:footer>
+            copy right.
+        </template>
+    </AlertModal>
+  </div>
+</template>
+
+<script>
+import AlertModal from './common/AlertModal.vue'
+
+export default {
+    data: function(){
+        return {
+            newTodoItem: "",
+            showModal: false
+        }
+    },
+    methods:{
+        addTodo: function(){
+          if(this.newTodoItem !== ''){
+              this.$emit('addTodoItem', this.newTodoItem);
+              this.clearInput();
+          } else {
+            this.showModal = !this.showModal;
+          }
+        },
+        clearInput: function(){
+            this.newTodoItem = "";
+        }
+    },
+    components:{
+      'AlertModal': AlertModal
+    }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
