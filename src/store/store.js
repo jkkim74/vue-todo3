e62f4af8e7603a53020428e57862ef552a846690
jack <jack68@naver.com>
2022-07-26 17:58:46 +0900

    axios add22

--- src/store/store.js
+++ src/store/store.js
@@ -41,24 +41,40 @@ export const store = new Vuex.Store({
     },
     mutations: {
         addOneItem(state,todoItem){
-            const obj = {completed: false, item: todoItem}
-            console.log("addOneItem");
+            const obj = {};
+            console.log(todoItem);
             //저장로직수행
-            localStorage.setItem(todoItem, JSON.stringify(obj));
+            //localStorage.setItem(todoItem, JSON.stringify(obj));
+            apiClient.put("/items/saveItem",{name: todoItem})
+            .then(res =>{
+                console.log(res.data);
+                obj.id = res.data.id;
+                obj.completed = res.data.completed;
+                obj.name = res.data.name;
+            }).catch(function(error){
+                console.log(error);
+            });
             //this.todoItems.push(obj);
+            console.log("===>"+obj);
             state.todoItems.push(obj);
         },
         removeOneItem(state,payload){
             console.log("removeOneItem");
-            localStorage.removeItem(payload.todoItem.item);
+            //localStorage.removeItem(payload.todoItem.item);
+            apiClient.delete("/items/delete/"+payload.todoItem.id)
+            .then(res =>{
+                console.log(res.data);
+            }).catch(function(error){
+                console.log(error);
+            });
             state.todoItems.splice(payload.index,1);
         },
         toggleOneItem(state,payload){
             console.log(state.todoItems[payload.index]);
             state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
             // localStorage는 업데이트가 없으므로 해당 ITEM을 삭제 후, 재정의하여 업데이트 처리를 한다..
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item,JSON.stringify(payload.todoItem));
+            //localStorage.removeItem(payload.todoItem.item);
+            //localStorage.setItem(payload.todoItem.item,JSON.stringify(payload.todoItem));
         },
         clearAllItems(state){
             localStorage.clear();
