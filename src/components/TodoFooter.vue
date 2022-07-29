aac7df36f8272d7cf52d7823ab4199d3f15975d0
jack <jack68@naver.com>
2022-07-29 15:57:32 +0900

    paging add

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="clearAllContainer">
+    <div class="pagination">
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a class="active" href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">&raquo;</a>
+    </div>
     <span class="clearAllBtn" v-on:click="clearTodo">
       Clear all
     </span>
   </div>
+  
 </template>
 
 <script>
@@ -20,7 +29,7 @@ export default {
 
 <style scoped>
 .clearAllContainer {
-  width: 8.5rem;
+  width: 24rem;
   height: 50px;
   line-height: 50px;
   background-color: white;
@@ -31,5 +40,24 @@ export default {
   color: #e20303;
   /* 추가 */
   display: block;
+}
+.pagination {
+  display: inline-block;
+}
+
+.pagination a {
+  color: black;
+  float: left;
+  padding: 4px 8px;
+  text-decoration: none;
+}
+
+.pagination a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
 }
 </style>
